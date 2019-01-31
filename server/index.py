e9ec7498f48cc372a1faea9934c9f2a93746c632
Meghana Singh <meghana.singh@bloc.io>
2019-01-31 08:42:46 -0800

    added request

--- server/index.py
+++ server/index.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from sqlalchemy import BigInteger, Column, Float, Integer, Text
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__,
 template_folder="../client/templates",
